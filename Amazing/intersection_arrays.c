34c8ab90f16e3fb7b2936470e5a22b2d76d8d116
akumarkk <akumarkk@outlook.com>
2014-12-25 23:29:35 -0700

    Complete implementation

--- Amazing/intersection_arrays.c
+++ Amazing/intersection_arrays.c
@@ -1,15 +1,122 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int
+get_frequency(int *a, int n, int key)
+{
+    int count = 0;
+    int low=0, high=n-1;
+    int mid, i;
 
+
+    while(low <= high)
+    {
+	mid = (low + high)/2;
+	if(a[mid] == key)
+	    break;
+	else if (key > a[mid])
+	    low  = mid+1;
+	else 
+	    high = mid - 1;
+    }
+
+    if(a[mid] == key)
+    {
+	count = 1;
+	for(i=mid+1; i<n; i++)
+	{
+	    if(a[i] == key)
+		count++;
+	    else
+		break;
+	}
+
+	for(i=mid-1; i>=0; i--)
+	{
+	    if(a[i] == key)
+		count++;
+	}
+    }
+
+    return count;
+}
+
+
+#define swap(a, b) { \
+    if(a != b){\
+	a = a ^ b;\
+	b = a ^ b;\
+	a = a ^ b;\
+    }\
+}
+
+int
+heapify(int *a, int start, int n)
+{
+    int lchild, rchild, largest;
+    int end = (n-1)/2;
+
+    while(start <= end)
+    {
+	lchild = 2*start + 1;
+	rchild = 2*start + 2;
+	if(rchild <= n)
+	{
+	    if(a[rchild] > a[lchild])
+		largest = rchild;
+	    else
+		largest = lchild;
+
+	}
+	else
+	    largest = lchild;
+
+	if(a[start] < a[largest])
+	{
+	    swap(a[start], a[largest]);
+	    start = largest;
+	}
+	else
+	    break;
+    }
+
+    return 0;
+}
+
+int
+build_max_heap(int *a, int n)
+{
+    for(int i = (n-1)/2; i>=0; i--)
+	heapify(a, i, n);
+
+    return 0;
+}
+
+
+   
+ int
+sort_set(int *a, int n)
+{
+    int i, tmp;
+
+    for(i=n-1; i>0;i--)
+    {
+	build_max_heap(a, i);
+	swap(a[i], a[0]);
+    }
+    return 0;
+}
+
+
+static int m;
 int **
 get_intersection(int **a, int num_sets, int *size)
 {
     int i=0, j=0;
-    int m, k, count, minset, minsize;
+    int k, count, minset, minsize;
     int **tmp;
 
-    for(i=0; i<numsets; i++)
+    for(i=0; i<num_sets; i++)
     {
 	if(minsize > size[i])
 	{
@@ -57,7 +164,7 @@ get_intersection(int **a, int num_sets, int *size)
 		else 
 		{
 		    tmp[0][m] = tmp[0][i];
-		    tmp[1][m] = (count < tmp[1][i])?count: mp[1][i];
+		    tmp[1][m] = (count < tmp[1][i])?count: tmp[1][i];
 		    m++;
 		}
 	     }
@@ -66,4 +173,37 @@ get_intersection(int **a, int num_sets, int *size)
 
      return 0;
 }
+
+
+int
+main()
+{
+    int n, **a, size[100];
+    int **tmp, i;
+
+    printf("# of sets : ");
+    scanf("%d", &n);
+
+    a = malloc(sizeof(int *) * n);
+
+    for(i=0; i<n; i++)
+    {
+	printf("Size : ");
+	scanf("%d", &size[i]);
+
+	a[i] = malloc(sizeof(int) * size[i]);
+	printf("Members of the set : ");
+	for(int j=0; j<size[i]; j++)
+	    scanf("%d", &a[i][j]);
+    }
+
+    tmp = get_intersection(a, n, size);
+    
+    for(i=0; i<m; i++)
+	printf("%d[%d]  ", tmp[0][i], tmp[1][i]);
+    
+    printf("\n");
+    return 0;
+}
+	    
 
