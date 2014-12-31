6d346ee1dff7ae256b4f14683daa2ffbad6dc176
akumarkk <akumarkk@outlook.com>
2014-12-31 04:13:16 -0700

    description of problem

--- Geeks/wave_sorting.c
+++ Geeks/wave_sorting.c
@@ -1,3 +1,21 @@
+/* Sort an array in wave form
+ * Given an unsorted array of integers, sort the array into a wave like array. An array ‘arr[0..n-1]’ is sorted in wave form if arr[0] >= arr[1] <= arr[2] >= arr[3] <= arr[4] >= …..
+ *
+ * Examples:
+ *
+ *  Input:  arr[] = {10, 5, 6, 3, 2, 20, 100, 80}
+ *   Output: arr[] = {10, 5, 6, 2, 20, 3, 100, 80} OR
+ *                    {20, 5, 10, 2, 80, 6, 100, 3} OR
+ *                                     any other array that is in wave form
+ *
+ *
+ * Solution :
+ * This can be done in O(n) time by doing a single traversal of given array. The idea is based on the fact that if we make sure that all even positioned (at index 0, 2, 4, ..) elements are greater than their adjacent element, we don’t need to worry about odd positioned element. Following are simple steps.
+ * 1) Traverse all even positioned elements of input array, and do following.
+ * ….a) If current element is smaller than previous element, swap previous and current.
+ * ….b) If current element is smaller than next element, swap next and current.
+ *
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
